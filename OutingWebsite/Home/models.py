2954c293c775ad05a7fc7b5eacfdd4dc3c4a2c94
sanjaynandy89 <sanjaynandy89@gmail.com>
2019-09-07 11:20:41 -0400

    Adding Workspace in local repository

---
+++ OutingWebsite/Home/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Packages(models.Model):
+   name = models.CharField('Event Name', max_length=120)
+   img=models.ImageField(upload_to='pics')
+   description = models.TextField(blank=True)
---
+++ OutingWebsite/Home/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+def HomePage(request):
+    return render(request, 'Home.html')
